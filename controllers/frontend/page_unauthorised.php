6c2f1ca5130ad8b537089eef79613dc31a945851
Atari <andrew.gerber@design-portfolio.co.uk>
2019-07-19 13:12:37 +0100

    Adds 401 (page unauthorised) frontend controller & view

---
+++ controllers/frontend/page_unauthorised.php
@@ -0,0 +1,11 @@
+<?php namespace Concrete\Controller\Frontend;
+
+use Controller;
+
+/**
+ * Class PageUnauthorised
+ */
+class PageUnauthorised extends Controller
+{
+    protected $viewPath = '/frontend/page_unauthorised';
+}
---
+++ views/frontend/page_unauthorised.php
@@ -0,0 +1,5 @@
+<?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
+
+<h1 class="error"><?= t('Page Unauthorised'); ?></h1>
+<p><?= t('You are not authorised to access this resource.'); ?></p>
+<a href="<?= DIR_REL; ?>/"><?= t('Back to Home'); ?></a>.
