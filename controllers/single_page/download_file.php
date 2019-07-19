80c91253cb6f3df1384e71b30ebee20e3d1b5f95
Atari <andrew.gerber@design-portfolio.co.uk>
2019-07-19 13:23:34 +0100

    Enhances download file single page controller & template

--- controllers/single_page/download_file.php
+++ controllers/single_page/download_file.php
@@ -1,149 +1,234 @@
-<?php
-namespace Concrete\Controller\SinglePage;
+<?php namespace Concrete\Controller\SinglePage;
 
-use PageController;
-use Core;
-use Page;
-use Permissions;
+use Concrete\Core\Http\Response;
 use Concrete\Core\Entity\File\File as FileEntity;
+use Concrete\Core\Entity\File\Version as FileVersion;
 use Concrete\Core\File\File;
+use Concrete\Core\Http\ResponseFactoryInterface;
+use Concrete\Core\Page\Controller\PageController;
+use Concrete\Core\Page\Page;
+use Concrete\Core\Permission\Checker as Permissions;
+use Concrete\Core\Routing\Redirect;
+use Concrete\Core\Routing\RedirectResponse;
+use Concrete\Core\User\User;
 
 class DownloadFile extends PageController
 {
-    protected $force = 0;
+
+    /* VARIABLES **************************************************************/
 
     /**
-     * @param int $fID File ID
+     * @var bool $force
+     */
+    protected $force = false;
+
+
+    /* HELPER METHODS *********************************************************/
+
+    /**
+     * @param int $fID
+     *
+     * @return FileEntity|null
+     */
+    protected function getFile($fID)
+    {
+        if ($fID > 0) {
+            return File::getByID($fID);
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the user has permissions to view the page the asset is related to.
+     *
+     * @note returns void, but sets `Page $rc` front-end variable
+     *
+     * @param int $rcID
+     *
+     * @return void
+     */
+    protected function canViewPage($rcID)
+    {
+        $rc = Page::getByID($rcID, 'ACTIVE');
+        if (is_object($rc) && !$rc->isError()) {
+            $rcp = new Permissions($rc);
+            if ($rcp->canViewPage()) {
+                $this->set('rc', $rc);
+            }
+        }
+    }
+
+    /**
+     * @param FileEntity $f
+     *
+     * @return mixed
+     */
+    protected function canViewFile(FileEntity $f)
+    {
+        $fp = new Permissions($f);
+        return $fp->canViewFile();
+    }
+
+    /**
+     * @param FileVersion $fv
+     *
+     * @return mixed
+     */
+    protected function checkDisclaimer(FileVersion $fv)
+    {
+        if ($fv->getAttribute('fDisclaimer') === true) {
+            $user = new User();
+            if (!$user->isRegistered()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Download the asset, or redirect if remotely located.
+     *
+     * @param FileEntity $file
      * @param null|int $rcID
+     *
+     * @return Response|RedirectResponse|void
+     */
+    protected function download(FileEntity $file, $rcID = null)
+    {
+        // track
+        $file->trackDownload($rcID);
+
+        // no force - check for public URL, else fallback
+        $fv = $file->getVersion();
+        if ($this->force === false) {
+            $fsl = $file->getFileStorageLocationObject();
+            $configuration = $fsl->getConfigurationObject();
+            if ($configuration->hasPublicURL()) {
+                return Redirect::url($fv->getURL())->send();
+            }
+        }
+
+        // fallback
+        return $fv->buildForceDownloadResponse();
+    }
+
+
+    /* ACTIONS ****************************************************************/
+
+    /**
+     * @param int $fID file Id
+     * @param int|null $rcID related content ID
+     *
+     * @return void|mixed
      */
     public function view($fID = 0, $rcID = null)
     {
-        // get the file
-        if ($fID > 0 && $this->app->make('helper/validation/numbers')->integer($fID)) {
-            $file = File::getByID($fID);
-            if ($file instanceof FileEntity && $file->getFileID() > 0) {
-                $rcID = $this->app->make('helper/security')->sanitizeInt($rcID);
-                if ($rcID > 0) {
-                    $rc = Page::getByID($rcID, 'ACTIVE');
-                    if (is_object($rc) && !$rc->isError()) {
-                        $rcp = new Permissions($rc);
-                        if ($rcp->canViewPage()) {
-                            $this->set('rc', $rc);
-                        }
-                    }
-                }
-                $fp = new Permissions($file);
-                if (!$fp->canViewFile()) {
-                    return false;
-                }
+        // sanitise
+        $sanitiser = $this->app->make('helper/security');
+        $fID = $sanitiser->sanitizeInt($fID);
+        $rcID = $sanitiser->sanitizeInt($rcID);
 
-                // if file password is blank, download
-                if (!$file->getPassword()) {
-                    if ($this->force) {
-                        return $this->force_download($file, $rcID);
-                    } else {
-                        return $this->download($file, $rcID);
-                    }
-                }
-                // otherwise show the form
-                $this->set('force', $this->force);
-                $this->set('rcID', $rcID);
-                $this->set('fID', $fID);
-                $this->set('filename', $file->getFilename());
-                $fre = $file->getFileResource();
-                $this->set('filesize', $fre->getSize());
+        // locate file to proceed
+        if ($file = $this->getFile($fID)) {
+
+            // can view related collection
+            if ($rcID !== null) {
+                $this->canViewPage($rcID); // sets 'rc' front end variable ??
             }
-        }
-    }
 
-    /**
-     * @param int $fID File ID
-     * @param null|int $rcID
-     */
-    public function force($fID = 0, $rcID = null)
-    {
-        $this->force = true;
-
-        return $this->view($fID, $rcID);
-    }
-
-    /**
-     * @param int $fID File ID
-     */
-    public function view_inline($fID = 0)
-    {
-        if ($fID > 0 && $this->app->make('helper/validation/numbers')->integer($fID)) {
-            $file = File::getByID($fID);
-            $fp = new Permissions($file);
-            if (!$fp->canViewFile()) {
+            // can view file
+            if (!$this->canViewFile($file)) {
                 return false;
             }
 
-            $fre = $file->getFileResource();
-            $fsl = $file->getFileStorageLocationObject()->getFileSystemObject();
-            $mimeType = $file->getMimeType();
-            header("Content-type: $mimeType");
-            echo $file->getFileContents();
-            $this->app->shutdown();
-        }
-    }
-
-    /**
-     * @param int $fID File ID
-     */
-    public function submit_password($fID = 0)
-    {
-        if ($fID > 0 && $this->app->make('helper/validation/numbers')->integer($fID)) {
-            $f = File::getByID($fID);
-
-            $rcID = $this->post('rcID');
-            $rcID = $this->app->make('helper/security')->sanitizeInt($rcID);
-
-            if ($f->getPassword() == $this->post('password')) {
-                if ($this->post('force')) {
-                    return $this->force_download($f);
-                } else {
-                    return $this->download($f);
-                }
+            // disclaimer
+            $fv = $file->getApprovedVersion();
+            if (!$this->checkDisclaimer($fv)) {
+                return $this->app->make(ResponseFactoryInterface::class)->unauthorised('');
             }
 
-            $this->set('error', t("Password incorrect. Please try again."));
-            $this->set('force', ($this->post('force') ? 1 : 0));
-
-            $this->view($fID, $rcID);
+            // password or download
+            if ($file->getPassword()) {
+                // show the password form
+                $this->set('fID', $fID);
+                $this->set('rcID', $rcID);
+                $this->set('force', $this->force);
+                $this->set('filename', $fv->getFilename());
+                $this->set('filesize', $fv->getSize());
+            } else {
+                return $this->download($file, $rcID);
+            }
         }
     }
 
     /**
-     * @param \Concrete\Core\Entity\File\File $file
-     * @param null|int $rcID
-     */
-    protected function download(\Concrete\Core\Entity\File\File $file, $rcID = null)
-    {
-        $filename = $file->getFilename();
-        $file->trackDownload($rcID);
-        $fsl = $file->getFileStorageLocationObject();
-        $configuration = $fsl->getConfigurationObject();
-        $fv = $file->getVersion();
-        if ($configuration->hasPublicURL()) {
-            return \Redirect::url($fv->getURL())->send();
-        } else {
-            return $fv->forceDownload();
-        }
-    }
-
-    /**
-     * Forces the download of a file and shuts down.
-     * Returns null if approved version wasn't found.
+     * Password checking function.
      *
-     * @param File $file
-     * @param null|int $rcID
+     * @param int $fID file ID
+     * @param int|null $rcID related content ID
+     *
+     * @return RedirectResponse|void
      */
-    protected function force_download($file, $rcID = null)
+    public function submit_password($fID = 0, $rcID = null)
     {
-        $file->trackDownload($rcID);
+        // sanitise input
+        $sanitiser = $this->app->make('helper/security');
+        $fID = $sanitiser->sanitizeInt($fID);
 
-        // Magic call to approved FileVersion
-        return $file->forceDownload();
+        // locate file to proceed
+        if ($file = $this->getFile($fID)) {
+
+            // related collection
+            if ($rcID !== null) {
+                $rcID = $sanitiser->sanitizeInt($rcID);
+            }
+
+            // check password
+            $password = $sanitiser->sanitizeString($this->post('password'));
+            if ($password === $file->getPassword()) {
+                $this->download($file, $rcID);
+            } else {
+                $this->set('error', t("Password incorrect. Please try again."));
+                $this->view($fID, $rcID);
+            }
+        }
+    }
+
+    /**
+     * Method used to inject an asset into the WYSIWYG editor.
+     *
+     * @param int $fID file ID
+     *
+     * @return bool
+     */
+    public function view_inline($fID = 0)
+    {
+        // sanitise input
+        $sanitiser = $this->app->make('helper/security');
+        $fID = $sanitiser->sanitizeInt($fID);
+
+        // locate file to proceed
+        if ($file = $this->getFile($fID)) {
+
+            // can view file
+            if (!$this->canViewFile($file)) {
+                return false;
+            }
+
+            // disclaimer
+            $fv = $file->getApprovedVersion();
+            if (!$this->checkDisclaimer($fv)) {
+                return false;
+            }
+
+            // output
+            $mimeType = $fv->getMimeType();
+            header("Content-type: $mimeType");
+            echo $fv->getFileContents();
+            $this->app->shutdown();
+        }
+
+        // default return
+        return false;
     }
 }
--- single_pages/download_file.php
+++ single_pages/download_file.php
@@ -1,39 +1,36 @@
-<?php 
-defined('C5_EXECUTE') or die('Access Denied.');
+<?php defined('C5_EXECUTE') or die('Access Denied.');
+
+/**
+ * @var int $fID
+ * @var int $rcID
+ * @var bool $force
+ * @var string $filename
+ * @var string $filesize
+ */
+
+$form = Core::make('helper/form');
 ?>
 
-<h1><?=t('Download File')?></h1>
-
-<?php
-if (!isset($filename)) {
-    ?>
-    <p><?=t('Invalid File.'); ?></p>
-    <?php
-} else {
-    ?>
-    <p><?=t('This file requires a password to download.')?></p>
-    <?php
-    if (isset($error)) {
-        ?>
-        <div class="ccm-error-response"><?=$error?></div>
-        <?php
-    } ?>
-	<form action="<?= View::url('/download_file', 'submit_password', $fID) ?>" method="post">
-        <?php
-        if (isset($force)) {
-            ?>
-            <input type="hidden" value="<?= $force ?>" name="force" />
-            <?php
-        }
-        ?>
-		<input type="hidden" value="<?= $rcID ?>" name="rcID" />
-		<label for="password"><?=t('Password')?>: <input type="password" name="password" /></label>
-		<br /><br />
-		<button type="submit"><?=t('Download')?></button>
-	</form>
-    <?php
-}
-
-if (isset($rc) && is_object($rc)) {
-    ?><p><a href="<?= (string) URL::to($rc)?>">&lt; <?=t('Back')?></a></p><?php
-}
+<div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-center">
+        <h1><?= t('Download File'); ?></h1>
+        <?php if (!isset($filename)): ?>
+            <p><?= t('Invalid File.'); ?></p>
+        <?php else: ?>
+            <p><?= t('This file requires a password to download.'); ?></p>
+            <?php if (isset($error)): ?>
+                <div class="ccm-error-response"><?= $error; ?></div>
+            <?php endif; ?>
+            <form class="" action="<?= View::url('/download_file', 'submit_password', $fID, $rcID); ?>" method="post">
+                <input type="hidden" name="force" value="<?= $force ? 1 : 0; ?>">
+                <div class="form-group">
+                    <?= $form->label('password', t('Password')); ?>
+                    <?= $form->password('password', null); ?>
+                </div>
+                <?php $backURL = (isset($rc) && $rc instanceof Page) ? $rc->getCollectionLink() : Page::getByID(HOME_UID)->getCollectionLink(); ?>
+                <a class="btn btn-default" href="<?= $backURL; ?>"><?= t('Cancel'); ?></a>
+                <button type="submit" class="btn btn-primary"><?= t('Download'); ?></button>
+            </form>
+        <?php endif; ?>
+    </div>
+</div>
