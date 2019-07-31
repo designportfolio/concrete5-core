35ccf61af972c866ae55925ef512ae91cba6bf70
Atari <andrew.gerber@design-portfolio.co.uk>
2019-07-31 09:33:47 +0100

    Simplifies page unauthorised core template

--- single_pages/page_unauthorised.php
+++ single_pages/page_unauthorised.php
@@ -1,16 +1,8 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
-$c = Page::getCurrentPage();
-$main = new Area('Main');
-?>
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<h1 class="error"><?= t('Unauthorised'); ?></h1>
 
-<?php if ($main->getTotalBlocksInArea($c) || $c->isEditMode()): ?>
-    <?php $main->display($c); ?>
-<?php else: ?>
-    <div class="row">
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 text-center">
-            <h1 class="error"><?= t('Unauthorised'); ?></h1>
-            <p><?= t('You are not authorised to access this resource.'); ?></p>
-            <a class="btn btn-primary" href="<?= DIR_REL; ?>/"><?= t('Back to Home'); ?></a>
-        </div>
-    </div>
-<?php endif; ?>
+<?= t('You are not authorised to access this resource.'); ?>
+<br/>
+<br/>
+
+<a href="<?= DIR_REL ?>/"><?= t('Back to Home'); ?></a>.
